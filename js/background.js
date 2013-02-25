5cb6ea8332187dcde525daed91d7ccd57c85ecde
kelukeshan0608@yahoo.com.cn <wanglingfei@wanglingfei.(none)>
2013-02-25 09:14:54 +0800

    commit all

---
+++ js/background.js
@@ -0,0 +1,349 @@
+﻿
+$(document).ready(function(){
+	$('#player')[0].volume  = 1;
+	list =[];
+	current = 0;
+	isPause = 0;
+	isLike = 0;
+	isLoop =0;
+	localStorage.isLrc = localStorage.isLrc ? localStorage.isLrc : 0;
+	localStorage.channel = localStorage.channel ? localStorage.channel : 0;
+	localStorage.chnList = localStorage.chnList ? localStorage.chnList : '';
+	localStorage.type = 'n';
+	lrc = {};
+	notifyLrc = '';
+	notification  ='';
+
+	var parseLrc = function(lrc){
+        lrc = lrc.split('\n');
+        var filter = /^((?:\[[\d.:]+?\])+?)([^\[\]]*)$/,
+        nLrc = {};
+
+        for (var i = 0, len = lrc.length, res, time ; i < len ; i += 1) {
+            res = lrc[i].match(filter);
+            if (res) {
+                time = res[1].slice(1, -1).split('][');
+                for (var j = 0, jLen = time.length, tmp ; j < jLen ; j += 1) {
+                    tmp = time[j].split(':');
+                    nLrc[Number(tmp[0])*60+Math.round(tmp[1])] = res[2];
+                }
+            }
+        }
+        return nLrc;
+	};
+	var getLrc = function(title,artist){
+		var result ;
+		$.ajax({
+            url: 'http://openapi.baidu.com/public/2.0/mp3/info/suggestion',
+            data: 'format=json&word='+encodeURIComponent(title.replace(/\(.+\)$/, ''))+'&callback=?',
+             complete: function (res) {
+             	//膈应接口返回数据的修整
+             	//新版Chrome Extension不让用eval,new Function ,inline script等等等等，各种不爽啊
+             	//xhr=>拿到数据，去掉多余的
+             	//unUnicode=>把中文Unicode的编码解码，并去掉用于解码的%
+             	//data=>JSON.parse转化为对象
+             	var xhr = JSON.stringify(res.responseText).slice(2,-3);
+             	xhr = xhr.replace(/\\\\\\\"/g, "'");
+             	var unUnicode =unescape(xhr.replace(/\\/g, "%")).replace(/%%%"\S+"/g,function(s){
+             		return s.replace(/"/g, "'").replace(/%/g, '');
+             	}).replace(/%/g, '');
+             	console.log(unUnicode);
+             	var data = JSON.parse(unUnicode)['song'];
+
+                if (!data) {return;}
+                for (var i = 0, len = data.length ; i < len ; i += 1) {
+                    if (artist.toLowerCase().indexOf(data[i].artistname.toLowerCase()) > -1) {
+                        $.ajax({
+                            url: 'http://ting.baidu.com/data/music/songlink',
+                            data: 'type=mp3&speed=&songIds=' + data[i].songid,
+                            dataType: 'json',
+                            success: function (data) {
+                                var song = data.data.songList[0];
+                               	list[current-1].songId = song.songId;
+                                if (song) {
+                                    $.get('http://ting.baidu.com'+song.lrcLink, function (client) {
+                                		list[current-1].lrc = parseLrc(client);
+                           					console.log(list[current-1]);
+                                    });
+                                }
+                            }
+                        });
+                        break;
+                    }
+                }
+             }
+        });
+	};
+
+	
+	var getList = function(type,channel,fn){
+		var data  =  "type="+type+"&channel="+channel+"&from=mainsite";
+		localStorage.type = type;
+		localStorage.channel = channel;
+		$.ajax({
+			url: 'http://douban.fm/j/mine/playlist',
+			type : 'get',
+			dataType: "json",
+	    	data: data,
+	    	success: function (data) {
+	    		console.log(data);
+	    		for(var i= 0;i<data.song.length;i++){
+	    			list.push(data.song[i]);
+	    		}	    		
+	            if (fn) {fn();}
+	        }
+	    });
+	};
+
+	var notifyFn = function(){
+		if(parseInt(localStorage.isLrc)){
+			if(notification){
+				currentStatus();
+				return;
+			}
+			//桌面通知初始化
+			notification = webkitNotifications.createHTMLNotification(
+		 		'notification.html' 
+			);					
+			notification.show();
+		}else{
+			if(notification){
+				notification.close();
+			}
+		}
+		currentStatus();
+		notification.onclose = function(){
+			notification = null;
+		};
+	};
+		
+	var playSongs = function(){
+		type = localStorage.type || 'n';
+		channel = localStorage.channel || 0 ;
+		if(list[current]!=undefined){
+			$('#player').attr('src',list[current].url);
+			//test 
+			setTimeout(function(){
+				$('#player')[0].play();
+			},50);
+			
+			current++;
+			getLrc(list[current-1].title,list[current-1].artist);
+			notifyFn();
+		}else{		
+			getList(type,channel,playSongs);
+		}
+		
+	};
+
+	chrome.extension.onConnect.addListener(function(fm) {
+		currentStatus = function(){
+			var info={};
+			if(list[current-1]){
+				if(list[current-1].like==1){
+					isLike = 1;
+				}else{
+					isLike = 0;
+				}
+				info ={
+					title : list[current-1].title,
+					albumtitle : list[current-1].albumtitle,
+					artist : list[current-1].artist,
+					albumUrl : list[current-1].album
+				};
+			}else if(list[0]&&current==0){
+				if(list[0].like==1){
+					isLike = 1;
+				}else{
+					isLike = 0;
+				}
+				info ={
+					title : list[0].title,
+					albumtitle : list[0].albumtitle,
+					artist : list[0].artist,
+					albumUrl : list[0].album
+				};
+			}
+			if(isPause){
+				chrome.browserAction.setIcon({path: 'icon_pause.png'});
+			}else{
+				chrome.browserAction.setIcon({path: 'icon.png'});
+			}
+		fm.postMessage({cmd: 'build',isPlay:1,pause:isPause,loop:isLoop,like:isLike,info:info,lrc:localStorage.isLrc,channel:localStorage.channel,chnList:localStorage.chnList});
+		};
+		fm.onMessage.addListener(function (msg) {
+			
+			type = localStorage.type;
+			channel = localStorage.channel;
+			inMiddle = msg.inMiddle;
+			
+			switch (msg.cmd) {
+				case 'check':
+				if($('#player').attr('src')!=''){
+					currentStatus();
+				}else{
+					fm.postMessage({cmd: 'build',isPlay:0});
+				}
+				break;
+				case 'play':
+				if(inMiddle){
+					$('#player')[0].play();	
+				}else{
+					getList(type,channel,playSongs);
+				}
+				chrome.browserAction.setIcon({path: 'icon.png'});
+				break;
+
+				case 'pause' :
+				$('#player')[0].pause();
+				chrome.browserAction.setIcon({path: 'icon_pause.png'});
+				break;
+
+				case 'loop' :
+				isLoop = 1;
+				break;
+
+				case 'unloop' :
+				isLoop = 0;
+				break;
+
+				case 'lrc' :
+				localStorage.isLrc = 1;
+				notifyFn();
+				break;
+
+				case 'unlrc' :
+				localStorage.isLrc = 0;
+				notifyFn();
+				break;
+				
+				case 'changeChannel':
+				localStorage.chnList = msg.chnList;
+				break;
+
+				case 'next':
+				isLoop = 0;
+				playSongs();
+				//由于歌词有延时，所以发个省略号
+				fm.postMessage({lrc:'……'},"notification.html");
+				break;
+
+				case 'prev':
+				isLoop = 0;
+				if(current-2 >= 0){
+					current = current-2;
+					playSongs();
+				}
+				fm.postMessage({lrc:'……'},"notification.html");
+				break;
+
+				case 'like' :
+				if(list[current-1]){
+					var sid = list[current-1].sid;
+				}
+				var data = "type=r&sid="+sid+"&channel="+localStorage.channel+"&from=mainsite";
+				$.ajax({
+					url: 'http://douban.fm/j/mine/playlist',
+					type : 'get',
+					dataType: "json",
+			    	data: data,
+			    	success: function (data) {
+			    		if(list[current-1]){
+			    			list[current-1].like = 1;
+			    		}
+			        }
+   				});
+				break;
+
+				case 'unlike' :
+				if(list[current-1]){
+					var sid = list[current-1].sid;
+				}
+				var data = "type=u&sid="+sid+"&channel="+localStorage.channel+"&from=mainsite";
+				$.ajax({
+					url: 'http://douban.fm/j/mine/playlist',
+					type : 'get',
+					dataType: "json",
+			    	data: data,
+			    	success: function (data) {
+			    		if(list[current-1]){
+			    			list[current-1].like = 0;
+			    		}
+			        }
+   				});
+				break;
+
+				case 'trash':
+				if(list[current-1]){
+					var sid = list[current-1].sid;
+				}
+				var data = "type=b&sid="+sid+"&channel="+localStorage.channel+"&from=mainsite";
+				$.ajax({
+					url: 'http://douban.fm/j/mine/playlist',
+					type : 'get',
+					dataType: "json",
+			    	data: data,
+			    	success: function (data) {
+			    		current--;
+			    		list.splice(current,1);
+			    		playSongs();
+			        }
+   				});
+				break;
+
+				case 'channel':
+				localStorage.channel = msg.channel;
+				list.splice(current,list.length-current+1);
+				getList(localStorage.type,localStorage.channel,playSongs);
+				break;
+
+				case 'share':
+				var song = list[current-1];
+				fm.postMessage({cmd: 'shareInfo',title:song.title,artist:song.artist,url:song.songId,pic:song.picture,from:msg.from});
+				break;
+
+				case 'notify':				
+				fm.postMessage({title:list[current-1].title+' by '+list[current-1].artist},"notification.html");
+				var curTime = parseInt($('#player')[0].currentTime);
+				var lrc = list[current-1].lrc;
+				if(!lrc){fm.postMessage({lrc:'……'},"notification.html");}
+				if(lrc && lrc[curTime] && notifyLrc != lrc[curTime]){
+					//因为popup页面关了，所以发送错误？？？如何监听popup页面？？
+					//notify 也是
+					fm.postMessage({lrc:lrc[curTime]},"notification.html");
+					//if(notification){
+					//	chrome.extension.getViews({type:"notification"}).inserLrc(lrc[curTime]);
+					//}
+					notifyLrc = lrc[curTime];
+				}
+				
+				break;
+			}
+		}.bind(this));
+
+		//根据歌曲时间循环 请求桌面提示页向background 请求歌词
+		$('#player')[0].addEventListener('timeupdate', function(event){	
+			fm.postMessage({getLrc:1});
+		}, false);
+
+		fm.onDisconnect.addListener(function (fm) {
+            fm = null;
+        }.bind(this));
+
+	});
+
+	$('#player')[0].addEventListener('ended', function(){	
+		if(isLoop){
+			current--;
+		}
+		playSongs();
+		currentStatus();
+	}, false);
+	$('#player')[0].addEventListener('pause', function(){
+		isPause = 1;
+	}, false);
+	$('#player')[0].addEventListener('play', function(){
+		isPause = 0;
+		//currentStatus();
+	}, false);
+});
---
+++ js/popup.js
@@ -0,0 +1,401 @@
+/*
+*1、bug:next prev pop页字不变
+*2、Uncaught Error: Attempting to use a disconnected port object 
+*3、优化UI
+*/
+var dfm = {};
+
+$(document).ready(function(){
+    defaultChanel = [-3,0,1,2,9,27,61];
+    
+    fm = chrome.extension.connect({name: 'fm'});
+
+	var isLogin = localStorage.isLogin;
+	
+	var new_img = function(){
+	 	$.ajax({
+	        url: 'http://douban.fm/j/new_captcha',
+	        type: 'get',
+	        success: function (data) {
+	        	var data = data.replace(/\"/g, "");
+	            $('#loginForm').find('[name=captcha_id]').val(data);
+	            $('#loginForm').find('img').attr('src', 'http://douban.fm/misc/captcha?size=m&id=' + data);
+	        }
+        });
+	};
+
+	var init = function(){
+		var channel = Number(localStorage.channel);
+     				
+		fm.onMessage.addListener(function (msg) {
+			switch (msg.cmd) {
+
+				case 'build':
+
+				if(!msg.isPlay){
+					fm.postMessage({cmd: 'play'});	
+				}	
+				if(msg.pause){
+					$('#play').html('P');
+					$('#play').attr('title','播放');
+				}else{
+					$('#play').html('7');
+					$('#play').attr('title','暂停');
+				}
+				if(msg.like){
+					$('#like').addClass('liking');
+				}else{
+					$('#like').removeClass('liking');
+				}
+				if(msg.loop){
+					$('#repeat').addClass('current');
+				}else{
+					$('#repeat').removeClass('current');
+				}
+				
+				if(Number(msg.lrc)){
+					$('#lrc').addClass('current');
+					$('#lrc').attr('title','隐藏歌词');
+				}else{
+					$('#lrc').removeClass('current');
+					$('#lrc').attr('title','显示歌词');
+				}
+				if(msg.info){
+					$('#cs_info').html(msg.info.artist+':&nbsp;&nbsp;'+msg.info.title+'<br/>Ablum:&nbsp;&nbsp;<a target="blank" href="http://music.douban.com'+msg.info.albumUrl+'">'+msg.info.albumtitle+'</a>');
+				}
+				
+				if(msg.chnList){
+					var list = msg.chnList.split(',');	
+					for(var i=0;i<list.length;i++){
+						var single = list[i].split('|');
+						var isHas = 0;
+						for(var j=0;j<$('#channel>option').length;j++){
+					    	if(parseInt($('#channel>option')[j].value,10) === parseInt(single[0],10)){
+					    		isHas = 1;
+					    		break;
+					    	}
+					    }
+					    if(!isHas){
+					    	$('#channel').append("<option value='"+single[0]+"'>"+single[1]+"</option>");
+					    }
+					}
+					
+					
+				}
+				if(msg.channel){
+					for(var i=0;i<$('#channel>option').length;i++){
+				    	if($('#channel>option')[i].value==msg.channel){
+				    		$('#channel>option')[i].selected = true;
+				    	}
+				    }
+				}
+				break;
+
+				case 'shareInfo' :
+				if(msg.title){
+					if(msg.from =='sina'){//appkey=1827380915
+						if(msg.url){
+							window.open('http://v.t.sina.com.cn/share/share.php?title=推荐音乐：【'+encodeURIComponent(msg.title)+"】 音乐人：【"+encodeURIComponent(msg.artist)+"】 收听地址：http://music.baidu.com/song/"+msg.url+'&content=utf-8&pic='+msg.pic);
+						}else{
+							window.open('http://v.t.sina.com.cn/share/share.php?title=推荐音乐：【'+encodeURIComponent(msg.title)+"】 音乐人：【"+encodeURIComponent(msg.artist)+"】 "+'&content=utf-8&pic='+msg.pic);
+						}
+						
+					}else if(msg.from =='douban'){
+						//window.open('http://widget.renren.com/dialog/share?resourceUrl='+msg.url+'&title=推荐音乐：【'+msg.title+'】 音乐人：【'+msg.artist+'】&pic='+msg.pic);
+					}
+				}			
+				break;
+			}
+		}.bind(this));
+	
+	};
+
+	if(!isLogin){
+		$('#fn').css('display','none');
+		$('#loginForm').css('display','block');
+		new_img();
+	}else{
+		$('#fn').css('display','block');
+		$('#loginForm').css('display','none');
+		init();
+		fm.postMessage({cmd: 'check'});
+	}
+
+	$('#play').click(function(event){
+		event.stopPropagation();
+		var channel = Number(localStorage.channel);
+		if($('#play').html()=='P'){
+			$('#play').text('7');
+			$('#play').attr('title','暂停');
+			fm.postMessage({cmd: 'play',type:'n',channel:channel,inMiddle:1});
+			return;
+		}else{
+			$('#play').text('P');
+			$('#play').attr('title','播放');
+			fm.postMessage({cmd: 'pause',type:'n',channel:channel});
+			return;
+		}
+		
+	});
+
+	$('#repeat').click(function(event){
+		event.stopPropagation();
+		if($('#repeat').hasClass('current')){
+			fm.postMessage({cmd: 'unloop'});
+			$('#repeat').removeClass('current');
+			$('#repeat').attr('title','循环');
+		}else{
+			fm.postMessage({cmd: 'loop'});
+			$('#repeat').addClass('current');
+			$('#repeat').attr('title','取消循环');
+		}
+		
+	});
+
+	$('#lrc').click(function(event){
+		event.stopPropagation();
+		if($('#lrc').hasClass('current')){
+			fm.postMessage({cmd: 'unlrc'});
+			$('#lrc').removeClass('current');
+			$('#lrc').attr('title','显示歌词');
+		}else{
+			fm.postMessage({cmd: 'lrc'});
+			$('#lrc').addClass('current');
+			$('#lrc').attr('title','隐藏歌词');
+		}
+	});
+
+
+	//chanel 删除事件绑定
+	var del  =function(event){
+		var target = event.target.parentNode;
+		var chnNumber = parseInt($(target).children('.num').text(),10);
+		for(var j=0;j<$('#channel>option').length;j++){
+	    	if(parseInt($('#channel>option')[j].value,10) === chnNumber){
+	    		$($('#channel>option')[j]).remove();
+	    		break;
+	    	}
+	    }
+		$(target).remove();
+	};
+	//搜索界面show
+	$('#changeChannel').click(function(){
+		$('#searchPanle').fadeIn();
+		$('#fn').hide();
+		$('#extra').empty();
+		$('#number').empty();
+
+		if(localStorage.chnList){
+			var list = localStorage.chnList.split(',');
+		}else{
+			return;
+		}
+		for(var i= 0;i<list.length;i++){
+			var one = list[i].split("|");
+			$('#extra').append('<li><span class="name">'+one[1]+'</span><span class="num">'+one[0]+'</span><span class="del">X</span></li>');
+		}
+		//删除频道事件绑定
+		$('.del').unbind().bind('click',del);
+	});
+
+	//search 输入框事件绑定
+	$('#search').bind('keydown',function(event){
+		if(event.keyCode === 13){
+			$.ajax({
+				url: 'http://douban.fm/j/explore/search',
+				type : 'get',
+				dataType: "json",
+		    	data: {
+		    		'query':$('#search').val(),
+		    		'start':0,
+		    		'limit':1
+		    	},
+		    	success: function (res) {
+		    		var data = res.data.channels[0];
+		    		if(!data){
+		    			$('#searchOne').html('sorry,no results.plz try again');
+		    			$('#add').hide();
+		    			return;
+		    		}
+		    		var name = data.name;
+		    		var intro = data.intro;
+		    		var id = data.id;
+		    		var pic = data.cover;
+		    		if($('#searchOne')){
+		    			$('#searchOne').remove();
+		    		}
+		    		$('#searchRes').prepend('<div id="searchOne"><img src="'+pic+'"/><span class="name">'+name+'</span><span class="intro">'+intro+'</span><span style="display:none" class="id">'+id+'</span></div>');
+
+		    		$('#add').show();
+		        }
+			});
+		}
+	});
+
+	//增加频道
+	$('#add').click(function(){
+		var num = parseInt($('#searchOne>.id').text(),10);
+		var name = $('#searchOne>.name').text();
+		var list = $('#extra').children();
+		var isHas = 0;
+		for(var j = 0;j<defaultChanel.length;j++){
+			if(defaultChanel[j] === num){
+				$('#searchOne').html("已存在");
+				$('#add').hide();
+				return;
+			}
+		}
+		for(var i= 0;i<list.length;i++){
+			if(parseInt($(list[i]).children('.num').text(),10) === num){
+				isHas = 1;
+				break;
+			}
+		}
+		if(!isHas){
+			$('#extra').append('<li><span class="name">'+name+'</span><span class="num">'+num+'</span><span class="del">X</span></li>');
+		}
+		//删除频道事件绑定
+		$('.del').unbind().bind('click',del);
+	});
+
+	//搜索保存并退出
+	$('#searchSave').click(function(){
+		var extra = $('#extra').children();
+		var list = [];
+
+		for(var i=0;i<extra.length;i++){
+			var chnNumber =  parseInt($(extra[i]).children('.num').text(),10);
+			var chnName  =$(extra[i]).children('.name').text();
+			var one = chnNumber+"|"+chnName;
+			var isHas = 0;
+			list.push(one);
+			for(var j=0;j<$('#channel>option').length;j++){
+		    	if(parseInt($('#channel>option')[j].value,10) === chnNumber){
+		    		isHas = 1;
+		    		break;
+		    	}
+		    }
+		    if(!isHas){
+		    	$('#channel').append("<option value='"+chnNumber+"'>"+chnName+"</option>");
+		    }
+	    }
+		fm.postMessage({cmd: 'changeChannel',chnList:list.join(',')});
+		$('#searchPanle').fadeOut('normal',function(){
+			$('#fn').show();
+		});
+		
+	});
+
+	$('#next').click(function(){
+		fm.postMessage({cmd: 'next'});
+	});
+
+	$('#prev').click(function(){
+		fm.postMessage({cmd: 'prev'});
+	});
+
+	$('#like').click(function(event){
+		event.stopPropagation();
+		if($('#like').hasClass('liking')){
+			fm.postMessage({cmd: 'unlike'});
+			$('#like').removeClass('liking');
+			$('#like').attr('title','喜欢');
+		}else{
+			fm.postMessage({cmd: 'like'});
+			$('#like').addClass('liking');
+			$('#like').attr('title','取消喜欢');
+		}
+	});
+
+	$('#trash').click(function(){
+		fm.postMessage({cmd: 'trash'});
+	});
+
+	$('#channel').change(function(){
+		var value = $('#channel').val();
+		fm.postMessage({cmd: 'channel',channel:value});
+	});
+
+	$('#shareSina').click(function(){
+		fm.postMessage({cmd: 'share',from:'sina'});
+	});
+
+
+	//绑定登陆表单事件
+	$('#username,#password,#captcha_solution').bind('focus',function(){
+		$(this).css('background','#fff');
+		$('#submit').css('background','#4CBEFF');
+		$('#tip').html('');
+	});
+	$('#username,#password,#captcha_solution').bind('blur',function(){
+		$(this).css('background','#E9F4E9');
+	});
+	//验证码换一换
+	$('#form_img').bind('click',new_img);
+	//提交
+	$('#submit').bind('click',function(e){
+		
+		var name = $('#username').val();
+		var pwd = $('#password').val();
+		if(name == ''||pwd == ''){
+			$('#tip').html('请输入完整信息进行登录！');
+			return;
+		}
+		$('#submit').css('background','#C7CACC');
+		$.ajax({
+			url: 'http://douban.fm/j/login',
+            type: 'post',
+            data: $('#loginForm').serialize(),
+            dataType: "json",
+            success: function (data) {
+            	if (data.r === 1) {
+                    new_img();
+                    $('#tip').html(data.err_msg);
+                }else if (data.r === 0) {
+                    chrome.cookies.get({
+                        url: 'http://douban.fm',
+                        name: 'dbcl2'
+                    }, function (c) {console.log(c)
+                        chrome.cookies.set({
+                            url: 'http://douban.com',
+                            name: 'dbcl2',
+                            value: c.value,
+                            domain: '.douban.com',
+                            path: '/',
+                            secure: c.secure,
+                            httpOnly: c.httpOnly,
+                            expirationDate: c.expirationDate,
+                            storeId: c.storeId
+                        });
+                    });
+                   // localStorage.username = name;
+                   // localStorage.password = pwd;
+                    localStorage.isLogin = true; 
+                    $('#loginForm').css('display','none');
+                    $('#fn').css('display','block');
+                    init();
+                    fm.postMessage({cmd: 'check'});
+                }
+            },
+            error: function (xhr, err) {
+                if(err === 'parsererror'){
+                	//此错误说明fm已登录，返回了跳转页面豆瓣主页
+                	//所以直接开启就可以了，有风险
+                	$('#tip').html('您已在豆瓣fm网站登录，将自动跳转！');
+                	setTimeout(function(){
+                		localStorage.isLogin = true; 
+	                    $('#loginForm').css('display','none');
+	                    $('#fn').css('display','block');
+	                    init();
+	                    fm.postMessage({cmd: 'check'});	
+                	},2000);	
+                }
+            }
+        });
+		e.preventDefault();
+	});
+	
+	
+	
+	
+});
